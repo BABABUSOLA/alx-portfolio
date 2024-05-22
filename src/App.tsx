cc640ba0d0659009ea074cfa45cbf35f4abb06bf
BABABUSOLA <queenbusola1@gmail.com>
2024-05-22 19:26:12 +0100

    fix: add sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Homepage from './components/homepage';
+import SignUp from './components/authentication/signup';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    
+    <SignUp />
   );
 }
 
