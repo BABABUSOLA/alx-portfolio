140e34337d43fdac2cc8d282ceb845079221cf07
BABABUSOLA <queenbusola1@gmail.com>
2024-05-22 21:35:21 +0100

    feat: add router, footer, hero page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import SignUp from './components/authentication/signup';
+import SignIn from './components/authentication/signin';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import LandingPage from './components/dashboard';
 
 function App() {
   return (
-    <SignUp />
+    <LandingPage />
   );
 }
 
